9575f090951f8b6ace79767dc994310be7e56154
TapZe <nabilmuyassar@Gmail.com>
2024-10-15 11:16:40 +0800

    chore: setting up api base url and query

---
+++ src/constants/apiBaseURI.tsx
@@ -0,0 +1,10 @@
+// Base url
+const BASE_URI = "https://fakestoreapi.com";
+
+// API query
+const PRODUCT_QUERY = `products`;
+const CART_QUERY = `carts`;
+const USER_QUERY = `users`;
+const LOGIN_QUERY = `auth/login`;
+
+export { BASE_URI, CART_QUERY, LOGIN_QUERY, PRODUCT_QUERY, USER_QUERY };
