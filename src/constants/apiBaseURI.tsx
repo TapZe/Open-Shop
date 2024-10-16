0a4a7c44105ef66cd53462ac04ec2b81420958d6
TapZe <nabilmuyassar@Gmail.com>
2024-10-16 14:30:01 +0800

    chore: setting up for product and product category fetching

--- src/constants/apiBaseURI.tsx
+++ src/constants/apiBaseURI.tsx
@@ -3,8 +3,16 @@ const BASE_URI = "https://fakestoreapi.com";
 
 // API query
 const PRODUCT_QUERY = `products`;
+const PRODUCT_CAT_QUERY = `${PRODUCT_QUERY}/categories`;
 const CART_QUERY = `carts`;
 const USER_QUERY = `users`;
 const LOGIN_QUERY = `auth/login`;
 
-export { BASE_URI, CART_QUERY, LOGIN_QUERY, PRODUCT_QUERY, USER_QUERY };
+export {
+  BASE_URI,
+  PRODUCT_CAT_QUERY,
+  CART_QUERY,
+  LOGIN_QUERY,
+  PRODUCT_QUERY,
+  USER_QUERY,
+};
