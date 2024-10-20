9c40750a2b79e973e453e1f9ac681816f3ecdaeb
TapZe <nabilmuyassar@Gmail.com>
2024-10-20 14:08:38 +0800

    feat: adding footer components

---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer footer-center bg-base-300 text-base-content p-4 md:px-10">
+        <aside>
+          <p>
+            Copyright © {new Date().getFullYear()} - All right reserved by
+            Muhammad Nabil Muyassar Rahman [1959095840-51].
+          </p>
+        </aside>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
