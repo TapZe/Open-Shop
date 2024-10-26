db38e6446f10f2918252fdf03efa1d3d7c884c87
TapZe <nabilmuyassar@Gmail.com>
2024-10-26 10:52:22 +0800

    chore: setting up pages

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -87,10 +87,7 @@ const Navbar: React.FC = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
+                <a className="justify-between">Profile</a>
               </li>
               <li>
                 <a>Logout</a>
