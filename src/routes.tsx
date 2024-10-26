3418e5dc372385218deb9aa162375af34ee55f02
TapZe <nabilmuyassar@Gmail.com>
2024-10-26 10:55:16 +0800

    chore: file management and setting up pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -10,7 +10,7 @@ import Fallback from "./components/Fallback";
 
 // Lazy-load when importing the pages
 const Home = lazy(() => import("./pages/Home"));
-const ProductDetail = lazy(() => import("./pages/ProductDetail"));
+const ProductDetail = lazy(() => import("./pages/product/ProductDetail"));
 
 const router = createBrowserRouter([
   {
