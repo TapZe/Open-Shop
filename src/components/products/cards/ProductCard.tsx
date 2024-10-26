a7f6b29041ebb2cd74928b4f58a4372bc0cc95e7
TapZe <nabilmuyassar@Gmail.com>
2024-10-26 12:25:26 +0800

    feat; add quantity to the card

--- src/components/products/cards/ProductCard.tsx
+++ src/components/products/cards/ProductCard.tsx
@@ -2,9 +2,19 @@ import { faHeart, faStar } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { productCardProps } from "../../../types/productTypes";
 import { Link } from "react-router-dom";
+import { RootState } from "../../../redux/store";
+import { useSelector } from "react-redux";
 
 const ProductCard: React.FC<productCardProps> = (props) => {
+  const { productQuantity } = useSelector(
+    (state: RootState) => state.persist.productQuantity
+  );
   const { id, title, price, rating, image, description } = props.product;
+
+  // Find the current product quantity
+  const productInCart = productQuantity.find((product) => product.id === id);
+  const quantity = productInCart ? productInCart.quantity : 0;
+
   return (
     <>
       <div className="card bg-base-100 w-72 shadow-xl transition-transform duration-300 transform hover:scale-105">
@@ -38,6 +48,9 @@ const ProductCard: React.FC<productCardProps> = (props) => {
             {title}
           </Link>
           <p className="truncate">{description}</p>
+          <div className="badge badge-primary badge-outline badge-lg">
+            Quantity: {quantity}
+          </div>
           <div className="card-actions justify-end mt-5">
             <button className="btn btn-primary">Add to Cart</button>
           </div>
