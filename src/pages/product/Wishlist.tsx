35b2209b0763d6b7fafd8f25b424ca648b39f56b
TapZe <nabilmuyassar@Gmail.com>
2024-10-27 13:24:23 +0800

    feat: wishlist page finished

--- src/pages/product/Wishlist.tsx
+++ src/pages/product/Wishlist.tsx
@@ -1,5 +1,40 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import ProductCard from "../../components/products/cards/ProductCard";
+import useAuthData from "../../hooks/useAuthData";
+import Fallback from "../../components/Fallback";
+
 const Wishlist = () => {
-  return <div></div>;
+  const { wishList } = useSelector(
+    (state: RootState) => state.persist.wishList
+  );
+  const { data: userData, isLoading } = useAuthData();
+  const wishData = userData ? wishList[userData.id] : [];
+
+  if (isLoading) {
+    return <Fallback />;
+  }
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center">
+        Saved <span className="text-cyan-600">Products</span>
+      </h1>
+      {!(wishData.length > 0) && (
+        <div role="alert" className="alert alert-info mb-5 my-10">
+          <FontAwesomeIcon icon={faCircleInfo} />
+          <span>No saved product yet!</span>
+        </div>
+      )}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 my-10">
+        {wishData?.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Wishlist;
