d20f54c0f31ea885bdfcbea7ab271be6c335a7c3
TapZe <nabilmuyassar@Gmail.com>
2024-10-31 16:22:38 +0800

    feat: theme controller auth, for now

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import ThemeBtn from "../components/layouts/buttons/ThemeBtn";
 
 const AuthLayout: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col overflow-hidden">
+      <div className="hidden">
+        <ThemeBtn />
+      </div>
       <main className="flex flex-grow sm:mx-2 md:mx-6 lg:mx-10 xl:mx-20 my-10 self-center items-center">
         <div className="w-full">
           <Outlet />
